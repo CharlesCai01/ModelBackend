ce573e7863854fa175bec04e47b702333ccf609b
RainOfPhone <792209198@qq.com>
2021-06-08 20:53:50 +0800

    fix: support that using gunicorn to start the server.

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 from pre_process import get_wait_ann_four_data
 
 
-content_list = get_wait_ann_four_data("./index2_wait.csv")
-print(content_list)
+def fake_res():
+    return [
+        ['O', 'B-Subject', 'B-Verb', 'O', 'O', 'O', 'B-Object', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'B-Subject', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'B-Subject', 'B-Subject', 'B-Verb', 'O', 'B-Object', 'B-Object', 'B-Object', 'B-Object', 'B-Object',
+         'B-Object', 'B-Object', 'B-Object', 'B-Object', 'B-Object', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+         'O', 'O', 'O'],
+
+    ]
